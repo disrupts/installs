f5739e11cc60199f3cc5f498d16ae4bd03841957
disrupts <disrupts@outlook.com>
2015-05-23 16:46:57 +0200

    Mac install fixes & new apps

--- mac.install.sh
+++ mac.install.sh
@@ -34,7 +34,7 @@ brew cask install                           \
   blender gimp                              \
   vmware-fusion                             \
   macvim                                    \
-  #musicbrainz-picard openemu
+  #musicbrainz-picard openemu musescore
  # liferea or qt replacement when pkgsrc will build it nicelly
  # trojita mail-client when I leave OS X
 
@@ -53,15 +53,21 @@ pip2 install --upgrade pip
 pip2 install flexget transmissionrpc
 
 # Python3 setup
+# scipy stuff
+pip3 install scipy numpy ipython matplotlib
+# pygame + opengl
 brew install sdl sdl_image sdl_mixer sdl_ttf sdl_net
-pip3 install scipy numpy ipython matplotlib pillow \
-  pyopengl pyopengl-accelerate                     \
-  cython                                           \
-  pygments
+pip3 install pyopengl pyopengl-accelerate pillow
 pip3 install hg+http://bitbucket.org/pygame/pygame
+# more
+pip3 install cython
+pip3 install pygments # pygmentize for colourled cat
+pip3 install nanpy    # arduino USB control!
+# screenplain doesn't work, replaced by textplay
+#pip install 'screenplain[PDF]' # screenwriting with fountain 
 
 # Ruby Gems
-gem install lolcat pry
+gem install lolcat pry textplay
 
 # X11 stuff
 brew cask install xquartz
